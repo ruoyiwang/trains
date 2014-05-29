0f1858855ccae09bf0bafa440565d2a67dd1536d
ruoyiwang <wang.ruoyi.uw@gmail.com>
2014-05-29 05:48:25 +0000

    adding few defines for RPS tasks

--- tasks/Tasks.h
+++ tasks/Tasks.h
@@ -3,9 +3,16 @@
 #ifndef __TASKS__
 #define __TASKS__
 
+#define SIGNUP 	0x20
+#define PLAY 	0x21
+#define QUIT	0x22
+
+#define RPS_SERVER_NAME		"RPS SERVER"
+
 void spawnedTask ();
 void FirstUserTask ();
 
 void nameServerTest1 ();
 void nameServerTest2 ();
+
 #endif
